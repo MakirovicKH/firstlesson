1a4bde327e2c858b1eb21f357e2be9944402b460
MakirovicKH <mahirrx-ap215@code.edu.az>
2024-10-03 16:31:20 +0400

    first commit

---
+++ FirstlessonProgramming/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+#region 
+//Verilmiş 3 rəqəmli ədədin rəqəmlərinin cəminin cüt olub-olmamasının tapılması.
+
+//int n = 123;
+//int sum = 0;
+//for (int i = n; i > 0; i = i / 10)
+//{
+//    n = n % 2;
+//    sum = sum + i % 10;
+//}
+//{
+//    if (n % 2 == 0)
+
+//}
+//Console.WriteLine(sum);
+//Console.ReadLine();
+
+
+//first lesson
+int n = 123;
+bool isEven = n % 2 == 0;
+
+//second lesson
+//int num = 4567;
+//    int minler = (num / 1000)%10;
+//    int yuzler = (num / 100);
+//    int onlar = (num / 10) % 10;
+//    int tekler = (num % 10);
+
+//double sum = (minler + yuzler + onlar + tekler) / 4.0;
+
+//Console.WriteLine(sum);
+
+//three lesson
+int num = 123;
+
+#endregion
